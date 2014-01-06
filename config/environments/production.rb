3033c15a676896e58d831837266cd313cc79e72e
Alexander Bassitt <alexander.bassitt@gmail.com>
2014-01-06 17:13:47 -0500

    Implemented Devise for Users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+# Note to set this to actual host
+config.action_mailer.default_url_options = { :host => 'http://meme-pinteresting.herokuapp.com/' }
+
 end
